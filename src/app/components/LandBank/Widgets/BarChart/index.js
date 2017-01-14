5d732e3255c777ebec4cfbb752f23b408b48a6b2
andrewmenendez <andrew@engrade.com>
2017-01-14 13:08:10 -0500

    tweaking bar chart

--- src/app/components/LandBank/Widgets/BarChart/index.js
+++ src/app/components/LandBank/Widgets/BarChart/index.js
@@ -1,7 +1,20 @@
 import React, { PropTypes, Component } from 'react';
 import classnames from 'classnames';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, BarChart, Bar  } from 'recharts';
+import { CartesianGrid, XAxis, YAxis, Tooltip, Legend, BarChart, Bar  } from 'recharts';
 
+import './style.css';
+
+const CustomizedAxisTick = React.createClass({
+  render () {
+    const {x, y, stroke, payload} = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+      </g>
+    );
+  }
+});
 
 export default class MyBarChart extends Component {
 
@@ -21,15 +34,15 @@ export default class MyBarChart extends Component {
 
     return (
           <div className={classnames('MyBarChart', className)}>
-          <span> Hi I'm a line graph</span>
+
             <BarChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
 
 
               <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="subdivision" name="subdivision"/>
+                <XAxis dataKey="subdivision" name="subdivision" height={60} tick={<CustomizedAxisTick/>} interval={0}/>
                 <YAxis name="Average Sales"/>
                 <Tooltip />
-                <Legend />
+                <Legend verticalAlign="top"/>
                 <Bar dataKey="averageSales" fill="#8884d8" />
                 <Bar dataKey="closedToDate" fill="#82ca9d" />
 
